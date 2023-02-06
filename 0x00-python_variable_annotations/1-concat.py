a5f81d135757e56845d03e3fb28050f9b21d3364
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-06 22:16:25 +0300

    creating concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+##!/usr/bin/env python3
+"""
+function: conca strings
+"""
+def concat(str1: str, str2: str) -> str:
+    """
+    Return concatnation on str1 and str2
+    """
+    return str1 + str2
