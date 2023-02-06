d19d5f6a54ab591d218030396023b181267166d9
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-06 21:30:45 +0300

    task1

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function add
+"""
+def add(a: float,b: float) -> float:
+    """
+    return addition on a and b,flaot
+    """
+
+    return a + b
+
+print(add(1.9,2.0))
+
