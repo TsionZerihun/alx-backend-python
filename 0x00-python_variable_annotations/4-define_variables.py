6687ca327646eef2effa339e58b0f585befadd68
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-07 11:11:15 +0300

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 """
 annotation example
 """
-=======
-#!/usr/bin/env python3
 
->>>>>>> d1f64411ee40669b216757fe900503f5af66af45
 
 a: int = 1
 pi: float = 3.14
