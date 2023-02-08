1db01e8251f8426dc01395751ed5d7b89ec821d1
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-08 09:41:36 +0300

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
     sum list function
 """
@@ -9,12 +10,12 @@ def sum_list(input_list: List[float]) -> float:
     """
         Args:
             input_list: float numbers
-
+            
         Return:
             Sum of the float numbers
     """
-
-    result: float = 0
+    
+   result: float = 0
 
     for x in input_list:
         result += x
