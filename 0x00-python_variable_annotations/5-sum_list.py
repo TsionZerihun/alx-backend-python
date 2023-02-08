ddc8b3525603413ebab467915d9ac62dcd4552ee
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-08 08:49:19 +0300

    create 5-sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/ur/bin/env python3
+"""
+sum list function
+"""
+
+
+def sum_list(input_list: list[float]):
+    sum: float = 0
+    for i in input_list:
+        sum += i
+        """
+        return sum of lists as float
+        """
+    return sum
