1fac5011abbcf24b557b54e0303ae93f05ebb755
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-08 10:54:29 +0300

    fix error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,8 +5,8 @@ tokv function
 from typing import Tuple, Union
 
 
-def to_kv(k: str,v: Union[int, float]) -> Tuple[int, float]]:
+def to_kv(k: str,v: Union[int, float]) -> Tuple[int, float]:
     """
     return tuple
     """
-    return(k,v)
+    return(k, v**2)
