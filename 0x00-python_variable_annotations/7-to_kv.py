3a17873fee0d0786c4eaaed365381391dac53b42
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-08 11:55:54 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ tokv function
 from typing import Tuple, Union
 
 
-def to_kv(k: str,v: Union[int, float]) -> Tuple[int, float]:
+def to_kv(k: str,v: Union[int, float]) -> Tuple[str, float]:
     """
     return tuple
     """
