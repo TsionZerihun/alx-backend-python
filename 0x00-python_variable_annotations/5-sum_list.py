3107723687c56ffbf484460daf74b571005cc09f
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-08 09:06:39 +0300

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,10 +5,13 @@ sum list function
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    return: sum of floats
+    """
+    
     sum: float = 0
+        
     for i in input_list:
         sum += i
-        """
-        return sum of lists as float
-        """
+        
     return sum
