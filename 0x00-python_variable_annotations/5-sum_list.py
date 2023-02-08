acff1a5bfbc230f070d3a6f8a0fe3fc2ac870c1e
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-08 09:03:11 +0300

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,9 @@
 """
 sum list function
 """
+from typing import List
 
-
-def sum_list(input_list: list[float]):
+def sum_list(input_list: List[float]) -> float:
     sum: float = 0
     for i in input_list:
         sum += i
