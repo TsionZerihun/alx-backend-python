d9fed7e5afbd3487a5daaaed57b33c4c91ecf0d2
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-11 08:04:23 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,7 @@ callable function
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     return a function
     """
