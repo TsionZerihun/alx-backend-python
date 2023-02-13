c58fe6811cc55118627f1d59a9628b8856d3da98
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-13 07:02:15 +0300

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,7 +5,7 @@ make element length
 from  typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence,int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns list
     """
