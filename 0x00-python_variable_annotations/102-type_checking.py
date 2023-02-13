4326af66fdf2d196b53b3aa123532c773749f597
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-13 08:37:21 +0300

    add task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,23 @@
-##/usr/bin/env python3
+##!/usr/bin/env python3
 """
-zoom array function
+Type Checking
 """
-from typing import List, Tuple
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
+from typing import Union, Any, Mapping, Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    zoom
+    """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, int(3.0))
