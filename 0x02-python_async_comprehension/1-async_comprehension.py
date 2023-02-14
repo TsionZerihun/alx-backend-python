747da0c50119ca0428146685b8d7d3d868b44b57
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-14 14:37:10 +0300

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-""" Async comprenhesion gen """
-import asyncio
+'''Task 1's module.
+'''
 from typing import List
+from importlib import import_module as using
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-        Generate numbers with async comprenhension
-
-        Args:
-            void
-
-        Return:
-            float random numbers
-    """
-    return ([i async for i in async_generator()])
+    '''Creates a list of 10 numbers from a 10-number generator.
+    '''
+    return [num async for num in async_generator()]
