c17b31f7fd0bf4ffa05e165e95e45b76579b0db5
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-14 14:20:23 +0300

    adding task3

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,19 @@
 ##!/usr/bin/env python3
+"""
+measure time function
+"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    calcualte and return toal time
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    total_time = time.time() - start_time
+    return (total_time)
