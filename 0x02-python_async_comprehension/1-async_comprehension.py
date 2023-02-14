6487cff0a0361783ad848dec57529d486915d423
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-14 14:08:45 +0300

    task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,15 @@
 ##!/usr/bin/env python3
+"""
+imports from task0
+"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Returts list of yiled
+    """
+    return ([yiled_item async for yiled_item in async_generator])
