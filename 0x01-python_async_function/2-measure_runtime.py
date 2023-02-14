326352fc3cc4c05acb295ca5092ea2c828a54d9b
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-14 09:30:32 +0300

    Update 2-measure_runtime.py
    updating measure time to normal function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,11 +9,11 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time (n: int, max_delay:int) -> float:
+def measure_time (n: int, max_delay: int) -> float:
     """
     returns average runtime
     """
-    start_time = time()
+    start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    total_time = time() - start_time 
+    total_time = time.time() - start_time 
     return total_time / n
