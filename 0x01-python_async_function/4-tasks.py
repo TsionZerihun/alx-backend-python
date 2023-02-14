2e02f80a4bcbbc5a545a433a0e6b97e557ea4036
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-14 11:57:35 +0300

    fix error

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,19 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
+"""
+wait like function
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n  addtimes with the specified max_delay
+    """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
