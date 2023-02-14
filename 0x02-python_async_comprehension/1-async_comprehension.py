a21cce15662735d16a45bbbed0ae49ff2e1e9aaf
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-14 14:33:56 +0300

    fix error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,20 @@
-##!/usr/bin/env python3
-"""
-imports from task0
-"""
+#!/usr/bin/env python3
+""" Async comprenhesion gen """
 import asyncio
 from typing import List
+
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """
-    Returts list of yiled
+        Generate numbers with async comprenhension
+
+        Args:
+            void
+
+        Return:
+            float random numbers
     """
-    return ([yiled_item async for yiled_item in async_generator])
+    return ([i async for i in async_generator()])
