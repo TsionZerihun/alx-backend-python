b7dfa0c2f68640ed990892306c1acdad987db2c5
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-02-14 13:54:55 +0300

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,6 +11,6 @@ async def async_generator() -> Generator[float, None, None]:
     '''
     yield a random number between 0 and 10
     '''
-        for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.random() * 10
+    for _ in range(10):
+    await asyncio.sleep(1)
+    yield random.random() * 10
